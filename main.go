1794c1d2018bdb2dc7d9a6e42ab7ad794c664d1c
king-open <kingwang.0624@gmail.com>
2022-11-20 02:22:29 +0800

    放弃 lorca，然后引入 gin

--- main.go
+++ main.go
@@ -1,13 +1,7 @@
 package main
 
 import (
-	"io/fs"
-	"log"
-	"net/http"
-	"os"
 	"os/exec"
-	"os/signal"
-	"strings"
 
 	"github.com/gin-gonic/gin"
 )
@@ -16,38 +10,14 @@ func main() {
 	go func() {
 		gin.SetMode(gin.DebugMode)
 		router := gin.Default()
-		staticFiles, _ := fs.Sub(FS, "frontend/dist")
-		router.StaticFS("/static", http.FS(staticFiles))
-		router.NoRoute(func(c *gin.Context) {
-			path := c.Request.URL.Path
-			if strings.HasPrefix(path, "/static/") {
-				reader, err := staticFiles.Open("index.html")
-				if err != nil {
-					log.Fatal(err)
-				}
-				defer reader.Close()
-				stat, err := reader.Stat()
-				if err != nil {
-					log.Fatal(err)
-				}
-				c.DataFromReader(http.StatusOK, stat.Size(), "text/html;charset=utf-8", reader, nil)
-			} else {
-				c.Status(http.StatusNotFound)
-			}
+		router.GET("/", func(c *gin.Context) {
+			c.Writer.Write([]byte("sdsd"))
 		})
 		router.Run(":8080")
 	}()
-
 	// 先写死路径，后面再照着 lorca 改
 	chromePath := "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe"
-	cmd := exec.Command(chromePath, "--app=http://127.0.0.1:8080/static/index.html")
+	cmd := exec.Command(chromePath, "--app=http://127.0.0.1:8080/")
 	cmd.Start()
-
-	chSignal := make(chan os.Signal, 1)
-	signal.Notify(chSignal, os.Interrupt)
-
-	select {
-	case <-chSignal:
-		cmd.Process.Kill()
-	}
+	select {}
 }
